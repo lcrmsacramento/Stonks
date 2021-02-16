43908f5757a76fcad765e398a1c81f34d68e4305
lcrmsacramento <luisacramento@hotmail.com>
2021-02-16 21:30:06 +0000

    fixed venv

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello, Flask!"
 
-app = Flask(__name__);
-
-@app.route("/<name>")              # at the end point /<name>
-def hello_name(name):              # call method hello_name
-    return "Hello " + name          # which returns "hello + name
-
-if __name__=="__main__":
-    app.run(debug=True);
+if __name__=='__main__':
+    app.run(debug=True)
