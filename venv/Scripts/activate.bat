778cd2bc667b53901de9757d6f3ea6a9641168a5
lcrmsacramento <luisacramento@hotmail.com>
2021-02-16 21:12:43 +0000

    updated venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lcrms\OneDrive\Documentos\dev\quirino\venv
+set VIRTUAL_ENV=C:\Users\lcrms\dev\Stonks\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
