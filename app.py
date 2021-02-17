a7cc9a45d518cd3ed71851ee30a22787e771ed80
lcrmsacramento <luisacramento@hotmail.com>
2021-02-17 02:23:16 +0000

    interactive graph

--- app.py
+++ app.py
@@ -1,60 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,abort, Response
 from flask_bootstrap import Bootstrap
-import json
+from flask import Flask, request, render_template
+import yfinance as yf
+
 app = Flask(__name__)
 Bootstrap(app)
-
-# define variables for data retrieval
-
 
 @app.route("/")
 def home():
     return render_template("index.html",title="stonks")
 
 
-def retrieve_data():
-    # create dictionary for saving current prices
-    current_prices = {}
-    for currency in currencies:
-        current_prices[currency] = []
-    # append new time to list of times
-    times.append(time.strftime('%H:%M:%S'))
+@app.route("/teste")
+def test():
+    return render_template("index.html")
 
-    # make request to API and get response as object
-    api_url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms=USD".format(",".join(currencies))
-    response = json.loads(requests.get(api_url).content)
 
-    # append new price to list of prices for graph
-    # and set current price for bar chart
-    for currency in currencies:
-        price = response[currency]['USD']
-        current_prices[currency] = price
-        prices[currency].append(price)
+@app.route("/stonks")
+def stonks():
+    return render_template("index.html")
 
-    # create an array of traces for graph data
-    graph_data = [go.Scatter(
-        x=times,
-        y=prices.get(currency),
-        name="{} Prices".format(currency)
-    ) for currency in currencies]
+# API Route for pulling the stock quote
+@app.route("/quote")
+def display_quote():
+	# get a stock ticker symbol from the query string
+	# default to AAPL
+	symbol = request.args.get('symbol', default="AAPL")
 
-    # create an array of traces for bar chart data
-    bar_chart_data = [go.Bar(
-        x=currencies,
-        y=list(current_prices.values())
-    )]
+	# pull the stock quote
+	quote = yf.Ticker(symbol)
 
-    data = {
-        'graph': json.dumps(list(graph_data), cls=plotly.utils.PlotlyJSONEncoder),
-        'bar_chart': json.dumps(list(bar_chart_data), cls=plotly.utils.PlotlyJSONEncoder)
-    }
+	#return the object via the HTTP Response
+	return quote.info
 
-    # trigger event
-    pusher.trigger("crypto", "data-updated", data)
+# API route for pulling the stock history
+@app.route("/history")
+def display_history():
+	#get the query string parameters
+	symbol = request.args.get('symbol', default="AAPL")
+	period = request.args.get('period', default="1y")
+	interval = request.args.get('interval', default="1mo")
+
+	#pull the quote
+	quote = yf.Ticker(symbol)	
+	#use the quote to pull the historical data from Yahoo finance
+	hist = quote.history(period=period, interval=interval)
+	#convert the historical data to JSON
+	data = hist.to_json()
+	#return the JSON in the HTTP response
+	return data
 
 
 if __name__=='__main__':
-    times = []
-    currencies = ["BTC"]
-    prices = {"BTC": []}
+    
     app.run(debug=True)
