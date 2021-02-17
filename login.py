fe95821e43a520b214c149d0d8a476a7615ec809
joao.sacramento <joao.sacramento@syone.com>
2021-02-17 00:47:47 +0000

    cenas

---
+++ login.py
@@ -0,0 +1,31 @@
+import flask-login as fl
+
+@app.route("/login")
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)   
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
